3afe09437215446c88f800378e9319a3f5cfc459
myntp <119732035+MYNTCODE@users.noreply.github.com>
2023-12-28 19:18:37 +0700

    Serve JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -1,39 +1,18 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
+console.log("Hello World");
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
+app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json",
+  });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
